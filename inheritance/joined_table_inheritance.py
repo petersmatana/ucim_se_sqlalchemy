ad57e7c20c1fbfea6e554b7cf89427683928cce7
Peter Smatana <petersmatana@gmail.com>
2017-08-03 09:51:05 +0200

    fix poly join connected to other 1:N entity

--- inheritance/joined_table_inheritance.py
+++ inheritance/joined_table_inheritance.py
@@ -5,6 +5,14 @@
     tady to hezky funguje, kdyz pridam engineera, svaze se tento zaznam
     i s tabulkou employee
 
+    !!! http://docs.sqlalchemy.org/en/latest/orm/mapper_config.html
+
+
+
+               poly join
+    employee ------------- enginees
+             ------------- manager  ---- N:1 ---- company
+
 '''
 
 
@@ -42,31 +50,24 @@ class Engineer(Employee):
     }
 
 
-# neco z tohoto: http://docs.sqlalchemy.org/en/latest/orm/mapper_config.html
-# fuuuuu!!.... :(
+class Company(Base):
+    __tablename__ = 'company'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    managers = relationship("Manager", back_populates="company")
 
-# http://docs.sqlalchemy.org/en/latest/orm/inheritance.html
 
 class Manager(Employee):
     __tablename__ = 'manager'
     id = Column(Integer, ForeignKey('employee.id'), primary_key=True)
     manager_name = Column(String(30))
 
-    # newtable_id = relationship("NewTable", back_populates="parent")
+    company_id = Column(ForeignKey('company.id'))
+    company = relationship("Company", back_populates="managers")
 
     __mapper_args__ = {
         'polymorphic_identity': 'manager',
     }
-
-
-'''
-class NewTable(Base):
-    __tablename__ = 'new_table'
-    label = Column(String(50))
-
-    parent_id = Column(Integer, ForeignKey('manager.id'))
-    parent = relationship("Manager", back_populates="child")
-'''
 
 
 Base.metadata.create_all(engine)
@@ -76,30 +77,20 @@ session = scoped_session(session_factory)
 
 
 def insert_data():
-    e = Engineer(name='smonty', engineer_name='eng name')
-    m = Manager(name='adam', manager_name='man name')
+    c = Company(name='fuuu')
+    c2 = Company(name='fiii')
 
-    session.add(e)
+    m = Manager(manager_name='asd', company=c)
+    m2 = Manager(manager_name='jojo', company=c2)
+    m3 = Manager(manager_name='llll', company=c2)
+
+    session.add(c)
+    session.add(c2)
+
     session.add(m)
+    session.add(m2)
+    session.add(m3)
 
     session.commit()
 
 insert_data()
-
-
-def select_data():
-    data = session.query(Engineer).filter_by(engineer_name='eng name').first()
-    print 'jmeno inzenyra = ', data.engineer_name
-
-
-select_data()
-
-
-def delete_data():
-    data = session.query(Engineer).filter_by(engineer_name='eng name').first()
-
-    session.delete(data)
-
-    session.commit()
-
-delete_data()
